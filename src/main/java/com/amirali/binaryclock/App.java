4bcd15798aa93ba4107217046e38735e8b416d94
amiran <azimi6370@gmail.com>
2024-01-10 13:39:34 +0330

    added: gradle workflow

--- src/main/java/com/amirali/binaryclock/App.java
+++ src/main/java/com/amirali/binaryclock/App.java
@@ -3,7 +3,10 @@ package com.amirali.binaryclock;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
 
@@ -13,6 +16,7 @@ public class App extends Application {
         final var loader = new FXMLLoader(getClass().getResource("clock-view.fxml"));
         stage.setScene(new Scene(loader.load(), 800, 600));
         stage.setOnCloseRequest(windowEvent -> System.exit(0));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("icons/icon.png")).toExternalForm()));
         stage.show();
     }
 }
