e42268ccea364158c6b4837d1b2fcbe8deedbdbb
amiran <azimi6370@gmail.com>
2024-01-12 11:41:24 +0330

    added: animation to button

--- src/main/java/com/amirali/binaryclock/App.java
+++ src/main/java/com/amirali/binaryclock/App.java
@@ -1,5 +1,6 @@
 package com.amirali.binaryclock;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -7,9 +8,10 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.Objects;
 
@@ -21,11 +23,11 @@ public class App extends Application {
 
         final var clock = new Clock();
 
-        final var githubRepoButton = new Button("Github by Amir Ali");
-        githubRepoButton.setCursor(Cursor.HAND);
+        final var githubRepoButton = createButton("Github by Amir Ali");
         githubRepoButton.setOnAction(actionEvent -> getHostServices().showDocument("https://github.com/AmirAli-AZ/BinaryClock"));
         githubRepoButton.getStyleClass().add("primary");
-        VBox.setMargin(githubRepoButton, new Insets(15, 0,0, 0));
+
+        VBox.setMargin(githubRepoButton, new Insets(15, 0, 0, 0));
 
         final var root = new VBox(5, clock, githubRepoButton);
         root.setId("root");
@@ -39,5 +41,23 @@ public class App extends Application {
         stage.show();
 
         clock.startTimer();
+    }
+
+    private Button createButton(String text) {
+        final var button = new Button(text);
+        button.setCursor(Cursor.HAND);
+        button.addEventHandler(MouseEvent.MOUSE_PRESSED, mouseEvent -> {
+            var scaleTransition = new ScaleTransition(Duration.millis(200), button);
+            scaleTransition.setToX(0.88);
+            scaleTransition.setToY(0.88);
+            scaleTransition.play();
+        });
+        button.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseEvent -> {
+            var scaleTransition = new ScaleTransition(Duration.millis(200), button);
+            scaleTransition.setToX(1);
+            scaleTransition.setToY(1);
+            scaleTransition.play();
+        });
+        return button;
     }
 }
